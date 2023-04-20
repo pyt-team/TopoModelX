6a7650818324fea40ae6fe0f2d4cdf97ae709d51
Nina Miolane <nmiolane@gmail.com>
2023-04-20 09:15:31 -0700

    Remove tests that belongs to tnx -> port it there

--- test/transforms/test_graph_to_simplicial_complex.py
+++ test/transforms/test_graph_to_simplicial_complex.py
@@ -2,28 +2,10 @@ import unittest
 
 import networkx as nx
 
-from torch_topo.topology import SimplicialComplex
-from torch_topo.transforms import (
+from topomodelx.transforms import (
     get_all_clique_complex_incidence_matrices,
     get_k_cliques,
 )
-
-
-def test_clique_complex():
-    G = nx.karate_club_graph()
-    cliques = list(nx.enumerate_all_cliques(G))
-    nodes = [i for i in cliques if len(i) == 1]
-    edges = [i for i in cliques if len(i) == 2]
-    faces = [i for i in cliques if len(i) == 3]
-    threefaces = [i for i in cliques if len(i) == 4]
-    fourfaces = [i for i in cliques if len(i) == 5]
-
-    SC = SimplicialComplex(cliques)
-    assert len(SC.get_simplices(0)) == len(nodes)
-    assert len(SC.get_simplices(1)) == len(edges)
-    assert len(SC.get_simplices(2)) == len(faces)
-    assert len(SC.get_simplices(3)) == len(threefaces)
-    assert len(SC.get_simplices(4)) == len(fourfaces)
 
 
 def test_get_k_cliques():
