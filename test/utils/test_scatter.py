2ec77db05a8bc8b84689208b6cdcaa09c92c8945
Mathilde Papillon <50878631+mathildepapillon@users.noreply.github.com>
2023-05-12 15:22:21 -0700

    Pull changes from t4est scatter

--- test/utils/test_scatter.py
+++ test/utils/test_scatter.py
@@ -6,38 +6,39 @@ from topomodelx.utils.scatter import scatter
 
 
 class TestScatter:
-  """Test the scatter module."""
-    
+    """Test the scatter module."""
+
     def test_scatter(self):
-      """Test the scatter function."""
-    
+        """Test the scatter function."""
         tests = [
             {
-                'src': [1., 3., 2., 4., 5., 6.],
-                'index': [0, 1, 0, 1, 1, 3],
-                'sum': [3., 12., 0., 6.],
-                'add': [3., 12., 0., 6.],
-                'mean': [1.5, 4., 0., 6.],
+                "src": [1.0, 3.0, 2.0, 4.0, 5.0, 6.0],
+                "index": [0, 1, 0, 1, 1, 3],
+                "sum": [3.0, 12.0, 0.0, 6.0],
+                "add": [3.0, 12.0, 0.0, 6.0],
+                "mean": [1.5, 4.0, 0.0, 6.0],
             },
             {
-                'src': [1.,1.,2., 2.],
-                'index': [0, 1, 0, 1],
-                'sum': [3.,3.],
-                'add': [3.,3.],
-                'mean': [1.5,1.5],
+                "src": [1.0, 1.0, 2.0, 2.0],
+                "index": [0, 1, 0, 1],
+                "sum": [3.0, 3.0],
+                "add": [3.0, 3.0],
+                "mean": [1.5, 1.5],
             },
             {
-                'src': [1.,2.,1.,2.,1.,2.,1.,2.],
-                'index': [0,0,0,0,1,1,2,2],
-                'sum': [6.,3.,3.],
-                'add': [6.,3.,3.],
-                'mean': [ 1.5 , 1.5, 1.5 ],
-            }
+                "src": [1.0, 2.0, 1.0, 2.0, 1.0, 2.0, 1.0, 2.0],
+                "index": [0, 0, 0, 0, 1, 1, 2, 2],
+                "sum": [6.0, 3.0, 3.0],
+                "add": [6.0, 3.0, 3.0],
+                "mean": [1.5, 1.5, 1.5],
+            },
         ]
-        for scat in ["add","sum","mean"]:
+        for scat in ["add", "sum", "mean"]:
             sc = scatter(scat)
-            for i in range(0,len(tests)):
-                computed=sc(torch.tensor(tests[i]['src']),
-                       torch.tensor(tests[i]['index']),
-                       dim=0)
-                assert (torch.all(computed == torch.tensor(tests[i][scat])))
+            for i in range(0, len(tests)):
+                computed = sc(
+                    torch.tensor(tests[i]["src"]),
+                    torch.tensor(tests[i]["index"]),
+                    dim=0,
+                )
+                assert torch.all(computed == torch.tensor(tests[i][scat]))
