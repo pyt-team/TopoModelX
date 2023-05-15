a3f4180da00c908644177f02c11af1baee6da419
Mathilde Papillon <50878631+mathildepapillon@users.noreply.github.com>
2023-05-15 11:26:41 -0700

    implemented test for template_layer

--- test/nn/simplicial/test_template_layer.py
+++ test/nn/simplicial/test_template_layer.py
@@ -1,1 +1,38 @@
 """Test the template layer."""
+import pytest
+import torch
+
+from topomodelx.nn.simplicial.template_layer import TemplateLayer
+
+
+class TestTemplateLayer:
+    """Test the template layer."""
+
+    @pytest.fixture
+    def template_layer(self):
+        """Return a template layer."""
+        in_channels = 10
+        intermediate_channels = 20
+        out_channels = 30
+        incidence_2 = torch.tensor([[1, 0, 1], [0, 1, 1]], dtype=torch.float32)
+        return TemplateLayer(
+            in_channels, intermediate_channels, out_channels, incidence_2
+        )
+
+    def test_forward(self, template_layer):
+        """Test the forward pass of the template layer."""
+        x = torch.randn(3, 10)
+        output = template_layer.forward(x)
+        assert output.shape == (3, 30)
+
+    def test_forward_with_invalid_input(self, template_layer):
+        """Test the forward pass of the template layer with invalid input."""
+        x = torch.randn(4, 10)
+        try:
+            template_layer.forward(x)
+            assert False, "Expected ValueError to be raised."
+        except ValueError as e:
+            assert (
+                str(e)
+                == f"Shape of input face features does not have the correct number of faces {template_layer.incidence_2.shape[-1]}."
+            )
