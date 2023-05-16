ba5fae79a223d34bce775da1681d093aa86d2082
Mathilde Papillon <50878631+mathildepapillon@users.noreply.github.com>
2023-05-16 15:59:39 -0700

    change neighboring mat to forward pass input

--- test/base/test_conv.py
+++ test/base/test_conv.py
@@ -21,17 +21,16 @@ class TestConv:
         neighborhood = torch.randint(0, 2, (n_cells, n_cells)).float()
 
         # Create a message passing convolution layer
-        mp_conv = Conv(
+        conv = Conv(
             in_channels=in_channels,
             out_channels=out_channels,
-            neighborhood=neighborhood,
             aggr_norm=True,
             update_func="sigmoid",
             initialization="xavier_uniform",
         )
 
         # Perform the forward pass
-        output = mp_conv(x)
+        output = conv.forward(x, neighborhood)
 
         # Check that the output has the correct shape
         expected_shape = (n_cells, out_channels)
@@ -39,3 +38,39 @@ class TestConv:
 
         # Check that the output values are within a reasonable range
         assert torch.all(output >= -1.0) and torch.all(output <= 1.0)
+
+
+# class TestConv:
+#     """Test the Conv class."""
+
+#     def test_message_passing_conv_forward(self):
+#         """Test the forward pass of the message passing convolution layer."""
+#         in_channels = 3
+#         out_channels = 5
+#         n_cells = 10
+
+#         # Create a random input tensor
+#         x = torch.randn((n_cells, in_channels))
+
+#         # Create a random neighborhood matrix (adjacency matrix)
+#         neighborhood = torch.randint(0, 2, (n_cells, n_cells)).float()
+
+#         # Create a message passing convolution layer
+#         mp_conv = Conv(
+#             in_channels=in_channels,
+#             out_channels=out_channels,
+#             neighborhood=neighborhood,
+#             aggr_norm=True,
+#             update_func="sigmoid",
+#             initialization="xavier_uniform",
+#         )
+
+#         # Perform the forward pass
+#         output = mp_conv(x)
+
+#         # Check that the output has the correct shape
+#         expected_shape = (n_cells, out_channels)
+#         assert output.shape == expected_shape
+
+#         # Check that the output values are within a reasonable range
+#         assert torch.all(output >= -1.0) and torch.all(output <= 1.0)
