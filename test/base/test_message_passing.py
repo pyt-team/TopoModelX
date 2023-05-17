08b1a23f2629c12a74d3eb395ebf8d06c7149268
Mustafa Hajij <mustafa.hajij@gmail.com>
2023-05-17 01:50:55 -0700

    small fixes

--- test/base/test_message_passing.py
+++ test/base/test_message_passing.py
@@ -1,13 +1,9 @@
-"""Test the message passing module."""
-
-import torch
-import numpy as np
 import unittest
 
+import numpy as np
+import torch
 
 from topomodelx.base.message_passing import MessagePassing
-
-import toponetx as tnx
 
 
 class TestMessagePassing(unittest.TestCase):
@@ -16,90 +12,76 @@ class TestMessagePassing(unittest.TestCase):
     def test_init(self):
         """Test the initialization of the message passing module."""
         mp = MessagePassing()
-    
+        print(mp)
 
     def test_forward(self):
         """Test the weights."""
-        a = np.array([[-1.,  0. ],
-                      [ 1.,- 1],
-                      [ 0.,  1.]])
-
+        a = np.array([[-1.0, 0.0], [1.0, -1], [0.0, 1.0]])
         mp = MessagePassing()
-        x = torch.rand(2,10)
-        x_out = mp(x,a)
-        expected = np.matmul(a,x.numpy())
-        np.testing.assert_array_almost_equal( x_out.numpy() ,expected)
+        x = torch.rand(2, 10)
+        x_out = mp(x, a)
+        expected = np.matmul(a, x.numpy())
+        np.testing.assert_array_almost_equal(x_out.numpy(), expected)
 
     def test__propagate(self):
-        """Test the _propagate function ."""
-        a = np.array([[-1.,  0. ],
-                      [ 1.,- 1],
-                      [ 0.,  1.]])
-        
-        a_ = np.array([[-1.,  0. ],
-                      [ 1.,- 0.5],
-                      [ 0.,  1.]])
+        """Test the _propagate function."""
+        a = np.array([[-1.0, 0.0], [1.0, -1], [0.0, 1.0]])
+        a_ = np.array([[-1.0, 0.0], [1.0, -0.5], [0.0, 1.0]])
         mp = MessagePassing()
-        x = torch.rand(2,10)
-        x_out = mp._propagate(x,a)
-        expected = np.matmul(a,x.numpy())
-        np.testing.assert_array_almost_equal( x_out.numpy() ,expected)
-       
+        x = torch.rand(2, 10)
+        x_out = mp._propagate(x, a)
+        expected = np.matmul(a, x.numpy())
+        np.testing.assert_array_almost_equal(x_out.numpy(), expected)
+
         # test without sign
-        x_out = mp._propagate(x,a,aggregate_sign=False)
-        expected = np.matmul(abs(a) ,x.numpy())
-        np.testing.assert_array_almost_equal( x_out.numpy() ,expected)
+        x_out = mp._propagate(x, a, aggregate_sign=False)
+        expected = np.matmul(abs(a), x.numpy())
+        np.testing.assert_array_almost_equal(x_out.numpy(), expected)
 
-
-        # test without value 
-        x_out = mp._propagate(x,a_, aggregate_value= False ) 
-        expected = np.matmul(a ,x.numpy()) 
-        np.testing.assert_array_almost_equal( x_out.numpy() ,expected) 
-
+        # test without value
+        x_out = mp._propagate(x, a_, aggregate_value=False)
+        expected = np.matmul(a, x.numpy())
+        np.testing.assert_array_almost_equal(x_out.numpy(), expected)
 
         # test without value and without sign
-        x_out = mp._propagate(x,a_, aggregate_sign=False, aggregate_value= False ) # vaules of a_ will be ignored
-        expected = np.matmul(abs(a) ,x.numpy()) 
-        np.testing.assert_array_almost_equal( x_out.numpy() ,expected)
-
-       
+        x_out = mp._propagate(
+            x, a_, aggregate_sign=False, aggregate_value=False
+        )  # vaules of a_ will be ignored
+        expected = np.matmul(abs(a), x.numpy())
+        np.testing.assert_array_almost_equal(x_out.numpy(), expected)
 
     def test_propagate(self):
-        """Test the propagate function ."""
-
-        a = np.array([[-1.,  0.],
-                      [ 1., -1.],
-                      [ 0.,  1.]])
-
-        b = np.array([[1., 1.],
-                      [1., 1.],
-                      [0., 1.]])
-
+        """Test the propagate function."""
+        a = np.array([[-1.0, 0.0], [1.0, -1.0], [0.0, 1.0]])
+        b = np.array([[1.0, 1.0], [1.0, 1.0], [0.0, 1.0]])
         mp = MessagePassing()
-        x = torch.rand(2,10)
-        x_out = mp.propagate(x,[a,b])
-        
-        expected_a = np.matmul(a,x.numpy())
-        expected_b = np.matmul(b,x.numpy())
-        
-        np.testing.assert_array_almost_equal( x_out[0].numpy() ,expected_a)
-        np.testing.assert_array_almost_equal( x_out[1].numpy() ,expected_b)
-  
-        
-        pass
+        x = torch.rand(2, 10)
+        x_out = mp.propagate(x, [a, b])
+        expected_a = np.matmul(a, x.numpy())
+        expected_b = np.matmul(b, x.numpy())
+        np.testing.assert_array_almost_equal(x_out[0].numpy(), expected_a)
+        np.testing.assert_array_almost_equal(x_out[1].numpy(), expected_b)
 
     def test_update(self):
+        """Test the update function."""
         pass
 
     def test_message(self):
+        """Test the message function."""
         pass
-    
+
     def test_aggregate(self):
+        """Test the aggregate function."""
         pass
+
     def test_get_i(self):
-        pass    
+        """Test the get_i function."""
+        pass
+
     def test_get_j(self):
-        pass 
-    
+        """Test the get_j function."""
+        pass
+
+
 if __name__ == "__main__":
-    unittest.main()    
+    unittest.main()
--- topomodelx/base/message_passing.py
+++ topomodelx/base/message_passing.py
@@ -1,16 +1,11 @@
 """Message passing module."""
-
 import numpy as np
-from scipy.sparse import coo_matrix
-from typing import List
-
 import torch
-import torch.nn as nn
+from scipy.sparse import coo_matrix
 from torch import Tensor
-from torch.nn.parameter import Parameter
-
 
 from topomodelx.utils.scatter import scatter
+
 
 class MessagePassing(torch.nn.Module):
     """MessagePassing.
@@ -26,138 +21,239 @@ class MessagePassing(torch.nn.Module):
     def forward(
         self, x: Tensor, a, aggregate_sign=True, aggregate_value=True
     ) -> Tensor:
-        """
-        Perform the forward pass of the message passing module.
-    
+        """Perform the forward pass of the message passing module.
+
         Parameters
         ----------
         x : Tensor
             Input features for each i-cell in the higher order domain.
         a : Tensor or a list of tensors
-            Neighborhood matrix representing the connections between i-targert cells and j- source cells.
-             One may think about this as cochain map a: C^j -> C^i, i.e. a map that 
-             sends signal supported on the j cells to a signal supported on i signal.
+            Neighborhood matrix representing the connections
+            between i-targert cells and j- source cells.
+             One may think about this as cochain map
+             a: C^j -> C^i, i.e. a map that
+             sends signal supported on the j cells to
+             a signal supported on i signal.
         aggregate_sign : bool, optional
-            Flag indicating whether to aggregate messages based on the sign of the neighborhood matrix values.
+            Flag indicating whether to aggregate messages
+            based on the sign of the neighborhood matrix values.
             Default is True.
         aggregate_value : bool, optional
-            Flag indicating whether to aggregate messages based on the values of the neighborhood matrix.
+            Flag indicating whether to aggregate messages
+            based on the values of the neighborhood matrix.
             Default is True.
-    
+
         Returns
         -------
         Tensor
-            Updated embeddings for each i-cell in the topological domain.    
-    
-        """      
-        
-        
+            Updated embeddings for each i-cell in the topological domain.
+
+        """
         return self.propagate(x, a, aggregate_sign, aggregate_value)
 
     def _propagate(self, x, a, aggregate_sign=True, aggregate_value=True) -> Tensor:
-        """
-        Perform message propagation in the topological domain.
-    
+        """Perform message propagation in the topological domain.
+
         Parameters
         ----------
         x : Tensor
             Input features for each i-cell in the topological domain.
         a : Tensor
-            Neighborhood matrix representing the connections between i-targert cells and j- source cells.
-             One may think about this as cochain map a: C^j -> C^i, i.e. a map that 
-             sends signal supported on the j cells to a signal supported on i signal.
+            Neighborhood matrix representing the connections
+            between i-targert cells and j- source cells.
+             One may think about this as cochain map a: C^j -> C^i,
+             i.e. a map that
+             sends signal supported on the j cells to a signal
+             supported on i signal.
         aggregate_sign : bool, optional
-            Flag indicating whether to aggregate messages based on the sign of the neighborhood matrix values.
+            Flag indicating whether to aggregate messages
+            based on the sign of the neighborhood matrix values.
             Default is True.
         aggregate_value : bool, optional
-            Flag indicating whether to aggregate messages based on the values of the neighborhood matrix.
+            Flag indicating whether to aggregate messages
+            based on the values of the neighborhood matrix.
             Default is True.
-    
+
+
         Returns
         ------
         Tensor
             Updated embeddings for each i-cell in the topological domain.
         Notes
         -----
-        This method performs message passing in the topological domain. 
+        This method performs message passing in the topological domain.
         It takes input features `x` and a neighborhood matrix `a`,
-        and computes messages from source cells to target cells based on the neighborhood matrix.
-        The messages are then aggregated based on the target indices defined by the neighborhood matrix,
-        and the aggregated messages are used to update the embeddings of the target cells.
-    
-        The `aggregate_sign` and `aggregate_value` flags control how the messages are aggregated.
-        If both flags are True, the messages are multiplied by the values of the neighborhood matrix.
-        If only `aggregate_sign` is True, the messages are multiplied by the sign of the
-        neighborhood matrix values. If only `aggregate_value` is True, the
-        messages are multiplied by the absolute values of the neighborhood matrix.
-    
-        The method returns the updated embeddings for i-cell in the topological domain.
-    
-        
-    """        
-        assert isinstance(x, Tensor) # x must be a torch tensor
+        and computes messages from source cells to target cells based
+        on the neighborhood matrix.
+        The messages are then aggregated based on the target
+        indices defined by the neighborhood matrix,
+        and the aggregated messages are used to update the
+        embeddings of the target cells.
 
-        if isinstance(a, (np.ndarray,np.matrix,coo_matrix)):
+        The `aggregate_sign` and `aggregate_value` flags control
+        how the messages are aggregated.
+        If both flags are True, the messages are multiplied by
+        the values of the neighborhood matrix.
+        If only `aggregate_sign` is True, the messages are
+        multiplied by the sign of the
+        neighborhood matrix values.
+        If only `aggregate_value` is True, the
+        messages are multiplied by the absolute values
+        of the neighborhood matrix.
+
+        The method returns the updated embeddings for
+        i-cell in the topological domain.
+
+
+        """
+        assert isinstance(x, Tensor)  # x must be a torch tensor
+
+        if isinstance(a, (np.ndarray, np.matrix, coo_matrix)):
             a = coo_matrix(a)
-            
-            self.index_i = torch.from_numpy(a.row.astype(np.int64)).to(torch.long) # Store the target indices for later use in aggregation
-            self.index_j = torch.from_numpy(a.col.astype(np.int64)).to(torch.long) # Store the source indices for later use in message passing
+
+            self.index_i = torch.from_numpy(a.row.astype(np.int64)).to(
+                torch.long
+            )  # Store the target indices for later use in aggregation
+            self.index_j = torch.from_numpy(a.col.astype(np.int64)).to(
+                torch.long
+            )  # Store the source indices for later use in message passing
             values = torch.from_numpy(a.data.astype(np.float64)).to(torch.float)
-        elif isinstance(a,Tensor):    
-            target, source = a.coalesce().indices()  # Get the indices of target and source cells
-            self.index_i = target  # Store the target indices for later use in aggregation
-            self.index_j = source  # Store the source indices for later use in message passing
+        elif isinstance(a, Tensor):
+            # Get the indices of target and source cells
+            (
+                target,
+                source,
+            ) = a.coalesce().indices()
+            # Store the target indices for later use in aggregation
+            self.index_i = target
+            # Store the source indices for later use in message passing
+            self.index_j = source
             values = a.coalesce().values()
         else:
-            TypeError("input tensor x must be a torch tonsor, numpy matrix or a coo matrix")
+            TypeError(
+                "input tensor x must be a torch tonsor,"
+                + "numpy matrix or a coo matrix"
+            )
         assert len(a.shape) == 2
         # each source cell sends messages
         # to its "neighbors" according to Nj
         # defined the operator a.
         messages = self.message(x)  # Compute messages from source cells
         if aggregate_sign and aggregate_value:
-            #values = a.coalesce().values()  # Get the values of the neighborhood matrix
-            messages = torch.mul(values.unsqueeze(-1), messages)  # Multiply values with messages
+            # values = a.coalesce().values()  # Get the values of the
+            # neighborhood matrix
+            messages = torch.mul(
+                values.unsqueeze(-1), messages
+            )  # Multiply values with messages
 
         elif aggregate_sign and not aggregate_value:
-            sign = torch.sign(values)  # Get the sign of the neighborhood matrix values
-            messages = torch.mul(sign.unsqueeze(-1), messages)  # Multiply sign with messages
+            # Get the sign of the neighborhood matrix values
+            sign = torch.sign(values)
+            messages = torch.mul(
+                sign.unsqueeze(-1), messages
+            )  # Multiply sign with messages
 
         elif aggregate_value and not aggregate_sign:
-            #values = a.coalesce().values()  # Get the values of the neighborhood matrix
-            messages = torch.mul(torch.abs(values.unsqueeze(-1)), messages)  # Multiply absolute values with messages
+            # values = a.coalesce().values()  # Get the values of the
+            # neighborhood matrix
+            messages = torch.mul(
+                torch.abs(values.unsqueeze(-1)), messages
+            )  # Multiply absolute values with messages
 
         # each target cell aggregates
         # messages from its neighbors according to Ni
         # defined via the operator a
-        embeddings = self.aggregate(messages)  # Aggregate messages from source cells based on target indices
+        embeddings = self.aggregate(
+            messages
+        )  # Aggregate messages from source cells based on target indices
 
         output = self.update(embeddings)  # Update target cell embeddings
 
         return output
-    
-    def propagate(self,x: Tensor , a, aggregate_sign=True, aggregate_value=True ) :
-        if isinstance(a,list): 
+
+    def propagate(self, x: Tensor, a, aggregate_sign=True, aggregate_value=True):
+        """Perform message passing operation.
+
+        Args:
+            x (Tensor): Input tensor.
+            a (Union[Tensor, coo_matrix, np.matrix, np.ndarray]):
+                Adjacency matrix or list of adjacency matrices.
+            aggregate_sign (bool, optional): Whether to aggregate
+            the signs of messages. Defaults to True.
+            aggregate_value (bool, optional): Whether to aggregate
+            the values of messages. Defaults to True.
+
+        Returns:
+            Union[Tensor, List[Tensor]]: Output tensor(s)
+            after message passing.
+        """
+        if isinstance(a, list):
             for i in a:
-                if not isinstance(i, (Tensor,coo_matrix,np.matrix,np.ndarray)):
-                    TypeError("each element in the list a must be Tensor,coo_matrix, np.ndarray or np.matrix ")
-            return [self.propagate(x,op) for op in a]
-        elif  isinstance(a, (Tensor,coo_matrix,np.matrix,np.ndarray)):
+                if not isinstance(i, (Tensor, coo_matrix, np.matrix, np.ndarray)):
+                    TypeError(
+                        "each element in the list a must"
+                        + "be Tensor,coo_matrix, np.ndarray or np.matrix "
+                    )
+            return [self.propagate(x, op) for op in a]
+        elif isinstance(a, (Tensor, coo_matrix, np.matrix, np.ndarray)):
             return self._propagate(x, a)
         else:
             TypeError(" input 'a' must be a list of 2d arrays or a 2d array")
-            
+
     def message(self, x: Tensor) -> Tensor:
+        """Extract features from source cells.
+
+        Args:
+            x (Tensor): Input tensor.
+
+        Returns:
+            Tensor: Extracted features from source cells.
+        """
         return self.get_j(x)  # Extract features from source cells
 
     def aggregate(self, messages: Tensor):
-        return self.agg(messages, self.index_i, 0)  # Perform aggregation of messages based on target indices
+        """Perform aggregation of messages based on target indices.
+
+        Args:
+            messages (Tensor): Input tensor containing messages.
+
+        Returns:
+            Tensor: Aggregated messages.
+        """
+        return self.agg(
+            messages, self.index_i, 0
+        )  # Perform aggregation of messages based on target indices
 
     def update(self, embeddings) -> Tensor:
+        """Return updated target cell embeddings.
+
+        Args:
+            embeddings (Tensor): Input tensor containing
+            target cell embeddings.
+
+        Returns:
+            Tensor: Updated target cell embeddings.
+        """
         return embeddings  # Return updated target cell embeddings
 
     def get_i(self, x: Tensor) -> Tensor:
-        return x.index_select(-2, self.index_i)  # Extract features from target cells
+        """Extract features from target cells.
+
+        Args:
+            x (Tensor): Input tensor.
+
+        Returns:
+            Tensor: Extracted features from target cells.
+        """
+        return x.index_select(-2, self.index_i)
 
     def get_j(self, x: Tensor) -> Tensor:
-        return x.index_select(-2, self.index_j)  # Extract features from source cells
+        """Extract features from source cells.
+
+        Args:
+            x (Tensor): Input tensor.
+
+        Returns:
+            Tensor: Extracted features from source cells.
+        """
+        return x.index_select(-2, self.index_j)
