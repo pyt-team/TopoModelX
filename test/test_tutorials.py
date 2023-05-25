d9b48f7d2e79196d8e23a0fade5f70ee68b3b734
Nina Miolane <nmiolane@gmail.com>
2023-05-25 07:20:26 -1000

    Update test_tutorials.py

--- test/test_tutorials.py
+++ test/test_tutorials.py
@@ -25,7 +25,7 @@ def _exec_tutorial(path):
     subprocess.check_call(args)
 
 
-paths = sorted(glob.glob("tutorials/cell/*.ipynb"))
+paths = sorted(glob.glob("tutorials/*.ipynb"))
 print(f"paths = {paths}")
 paths.extend(sorted(glob.glob("tutorials/hypergraph/*.ipynb")))
 print(f"paths = {paths}")
