df3f28b191db18073e6343ca4bd96f9e23b980df
Mustafa Hajij <mustafa.hajij@gmail.com>
2023-06-01 14:51:34 -0700

    fixing pyproject

--- topomodelx/embed/__init__.py
+++ topomodelx/embed/__init__.py
@@ -1,7 +1,7 @@
 __version__ = "0.0.1"
 
-from .classes.cell2vec import Cell2Vec
-from .classes.cell_diff2vec import CellDiff2Vec
-from .classes.deepcell import DeepCell
-from .classes.hoglee import HOGLEE
-from .classes.higher_order_laplacian_eigenmaps import HigherOrderLaplacianEigenmaps
+from .cell2vec import Cell2Vec
+from .cell_diff2vec import CellDiff2Vec
+from .deepcell import DeepCell
+from .hoglee import HOGLEE
+from .higher_order_laplacian_eigenmaps import HigherOrderLaplacianEigenmaps
