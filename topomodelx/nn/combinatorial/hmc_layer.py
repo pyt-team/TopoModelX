c061624bbf8a3c948d55643dee7100299c31affb
Rubén Ballester <rballeba@gmail.com>
2023-06-02 17:57:28 +0200

    Removing changes from comments on the dimensions in message passing

--- topomodelx/nn/combinatorial/hmc_layer.py
+++ topomodelx/nn/combinatorial/hmc_layer.py
@@ -5,9 +5,9 @@ from topomodelx.base.aggregation import Aggregation
 import torch.nn.functional as F
 from typing import List
 
-class HMCLayer(torch.nn.Module):
-    def __init__(self, in_channels: List[int], intermediate_channels : List[int], out_channels: List[int]):
 
+class HMCLayer(torch.nn.Module):
+    def __init__(self, in_channels: List[int], intermediate_channels: List[int], out_channels: List[int]):
         super().__init__()
         assert len(in_channels) == 3 and len(intermediate_channels) == 3 and len(out_channels) == 3
         in_channels_0, in_channels_1, in_channels_2 = in_channels
@@ -47,9 +47,7 @@ class HMCLayer(torch.nn.Module):
 
         self.aggr = Aggregation(aggr_func="sum", update_func="sigmoid")
 
-
         def forward(x_0, x_1, x_2, adjacence_0, adjacence_1, coadjacence_2, incidence_1, incidence_2):
-
             incidence_1_transpose = incidence_1.to_dense().T.to_sparse()
             incidence_2_transpose = incidence_2.to_dense().T.to_sparse()
 
@@ -59,7 +57,7 @@ class HMCLayer(torch.nn.Module):
             x_1_level1 = self.aggr([self.conv_level1_0_to_1(x_0, incidence_1_transpose),
                                     self.conv_level1_2_to_1(x_2, incidence_2)])
 
-            x_2_level1 = self.conv_level1_1_to_2(x_1,incidence_2_transpose)
+            x_2_level1 = self.conv_level1_1_to_2(x_1, incidence_2_transpose)
 
             x_0_level2 = self.conv_level2_0_to_0(x_0_level1, adjacence_0)
 
