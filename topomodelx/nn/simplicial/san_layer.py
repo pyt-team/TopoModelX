c670e349a7188ef10cb1f910f1e73c29b831dfd8
Indro Spinelli <indro.spinelli@gmail.com>
2023-06-06 17:06:01 +0200

    up

--- topomodelx/nn/simplicial/san_layer.py
+++ topomodelx/nn/simplicial/san_layer.py
@@ -1,88 +1,96 @@
+"""High Skip Network Layer."""
 import torch
 
+from topomodelx.base.aggregation import Aggregation
+from topomodelx.base.conv import Conv
 
-class SANLayer(torch.nn.Module):
-    """Layer of a Convolutional Cell Complex Network (CCXN).
 
-    Implementation of a simplified version of the CCXN layer proposed in [HIZ20]_.
+class HSNLayer(torch.nn.Module):
+    """Layer of a High Skip Network (HSN).
 
-    This layer is composed of two convolutional layers:
-    1. A convolutional layer sending messages from nodes to nodes.
-    2. A convolutional layer sending messages from edges to faces.
-    Optionally, attention mechanisms can be used.
+    Implementation of the HSN layer proposed in [HRGZ22]_.
 
     Notes
     -----
-    This is the architecture proposed for entire complex classification.
+    This is the architecture proposed for node classification on simplicial complices.
 
     References
     ----------
-    .. [HIZ20] Hajij, Istvan, Zamzmi. Cell Complex Neural Networks.
-        Topological Data Analysis and Beyond Workshop at NeurIPS 2020.
-        https://arxiv.org/pdf/2010.00743.pdf
+    .. [HRGZ22] Hajij, Ramamurthy, Guzmán-Sáenz, Zamzmi.
+        High Skip Networks: A Higher Order Generalization of Skip Connections.
+        Geometrical and Topological Representation Learning Workshop at ICLR 2022.
+        https://openreview.net/pdf?id=Sc8glB-k6e9
 
     Parameters
     ----------
-    in_channels_0 : int
-        Dimension of input features on nodes (0-cells).
-    in_channels_1 : int
-        Dimension of input features on edges (1-cells).
-    in_channels_2 : int
-        Dimension of input features on faces (2-cells).
-    att : bool
-        Whether to use attention.
+    channels : int
+        Dimension of features on each simplicial cell.
+    initialization : string
+        Initialization method.
     """
 
-    def __init__(self, in_channels_0, in_channels_1, in_channels_2, att=False):
+    def __init__(
+        self,
+        channels,
+    ):
         super().__init__()
-        self.conv_0_to_0 = Conv(
-            in_channels=in_channels_0, out_channels=in_channels_0, att=att
+        self.channels = channels
+
+        self.conv_level1_0_to_0 = Conv(
+            in_channels=channels,
+            out_channels=channels,
+            update_func="sigmoid",
         )
-        self.conv_1_to_2 = Conv(
-            in_channels=in_channels_1, out_channels=in_channels_2, att=att
+        self.conv_level1_0_to_1 = Conv(
+            in_channels=channels,
+            out_channels=channels,
+            update_func="sigmoid",
         )
 
-    def forward(self, x_0, x_1, neighborhood_0_to_0, neighborhood_1_to_2, x_2=None):
+        self.conv_level2_0_to_0 = Conv(
+            in_channels=channels,
+            out_channels=channels,
+            update_func=None,
+        )
+        self.conv_level2_1_to_0 = Conv(
+            in_channels=channels,
+            out_channels=channels,
+            update_func=None,
+        )
+
+        self.aggr_on_nodes = Aggregation(aggr_func="sum", update_func="sigmoid")
+
+    def reset_parameters(self):
+        r"""Reset learnable parameters."""
+        self.conv_level1_0_to_0.reset_parameters()
+        self.conv_level1_0_to_1.reset_parameters()
+        self.conv_level2_0_to_0.reset_parameters()
+        self.conv_level2_1_to_0.reset_parameters()
+
+    def forward(self, x_0, incidence_1, adjacency_0):
         r"""Forward pass.
 
-        The forward pass was initially proposed in [HIZ20]_.
+        The forward pass was initially proposed in [HRGZ22]_.
         Its equations are given in [TNN23]_ and graphically illustrated in [PSHM23]_.
-
-        The forward pass of this layer is composed of two steps.
-
-        1. The convolution from nodes to nodes is given by an adjacency message passing scheme (AMPS):
-
-        ..  math::
-            \begin{align*}
-            &🟥 \quad m_{y \rightarrow \{z\} \rightarrow x}^{(0 \rightarrow 1 \rightarrow 0)}
-                = M_{\mathcal{L}_\uparrow}(h_x^{(0)}, h_y^{(0)}, \Theta^{(y \rightarrow x)})\\
-            &🟧 \quad m_x^{(0 \rightarrow 1 \rightarrow 0)}
-                = \text{AGG}_{y \in \mathcal{L}_\uparrow(x)}(m_{y \rightarrow \{z\} \rightarrow x}^{0 \rightarrow 1 \rightarrow 0})\\
-            &🟩 \quad m_x^{(0)}
-                = m_x^{(0 \rightarrow 1 \rightarrow 0)}\\
-            &🟦 \quad h_x^{t+1,(0)}
-                = U^{t}(h_x^{(0)}, m_x^{(0)})
-            \end{align*}
-
-        2. The convolution from edges to faces is given by cohomology message passing scheme, using the coboundary neighborhood:
 
         .. math::
             \begin{align*}
-            &🟥 \quad m_{y \rightarrow x}^{(r' \rightarrow r)}
-                = M^t_{\mathcal{C}}(h_{x}^{t,(r)}, h_y^{t,(r')}, x, y)\\
-            &🟧 \quad m_x^{(r' \rightarrow r)}
-                = \text{AGG}_{y \in \mathcal{C}(x)} m_{y \rightarrow x}^{(r' \rightarrow r)}\\
-            &🟩 \quad m_x^{(r)}
-                = m_x^{(r' \rightarrow r)}\\
-            &🟦 \quad h_{x}^{t+1,(r)}
-                = U^{t,(r)}(h_{x}^{t,(r)}, m_{x}^{(r)})
+            &🟥 \quad m_{{y \rightarrow z}}^{(0 \rightarrow 0)} = \sigma ((A_{\uparrow,0})_{xy} \cdot h^{t,(0)}_y \cdot \Theta^{t,(0)1})\\
+            &🟥 \quad m_{z \rightarrow x}^{(0 \rightarrow 0)}  = (A_{\uparrow,0})_{xy} \cdot m_{y \rightarrow z}^{(0 \rightarrow 0)} \cdot \Theta^{t,(0)2}\\
+            &🟥 \quad m_{{y \rightarrow z}}^{(0 \rightarrow 1)}  = \sigma((B_1^T)_{zy} \cdot h_y^{t,(0)} \cdot \Theta^{t,(0 \rightarrow 1)})\\
+            &🟥 \quad m_{z \rightarrow x)}^{(1 \rightarrow 0)}  = (B_1)_{xz} \cdot m_{z \rightarrow x}^{(0 \rightarrow 1)} \cdot \Theta^{t, (1 \rightarrow 0)}\\
+            &🟧 \quad m_{x}^{(0 \rightarrow 0)}  = \sum_{z \in \mathcal{L}_\uparrow(x)} m_{z \rightarrow x}^{(0 \rightarrow 0)}\\
+            &🟧 \quad m_{x}^{(1 \rightarrow 0)}  = \sum_{z \in \mathcal{C}(x)} m_{z \rightarrow x}^{(1 \rightarrow 0)}\\
+            &🟩 \quad m_x^{(0)}  = m_x^{(0 \rightarrow 0)} + m_x^{(1 \rightarrow 0)}\\
+            &🟦 \quad h_x^{t+1,(0)}  = I(m_x^{(0)})
             \end{align*}
 
         References
         ----------
-        .. [HIZ20] Hajij, Istvan, Zamzmi. Cell Complex Neural Networks.
-            Topological Data Analysis and Beyond Workshop at NeurIPS 2020.
-            https://arxiv.org/pdf/2010.00743.pdf
+        .. [HRGZ22] Hajij, Ramamurthy, Guzmán-Sáenz, Zamzmi.
+            High Skip Networks: A Higher Order Generalization of Skip Connections.
+            Geometrical and Topological Representation Learning Workshop at ICLR 2022.
+            https://openreview.net/pdf?id=Sc8glB-k6e9
         .. [TNN23] Equations of Topological Neural Networks.
             https://github.com/awesome-tnns/awesome-tnns/
         .. [PSHM23] Papillon, Sanborn, Hajij, Miolane.
@@ -91,35 +99,25 @@ class SANLayer(torch.nn.Module):
 
         Parameters
         ----------
-        x_0 : torch.Tensor, shape=[n_0_cells, channels]
-            Input features on the nodes of the cell complex.
-        x_1 : torch.Tensor, shape=[n_1_cells, channels]
-            Input features on the edges of the cell complex.
-        neighborhood_0_to_0 : torch.sparse
-            shape=[n_0_cells, n_0_cells]
-            Neighborhood matrix mapping nodes to nodes (A_0_up).
-        neighborhood_1_to_2 : torch.sparse
-            shape=[n_2_cells, n_1_cells]
-            Neighborhood matrix mapping edges to faces (B_2^T).
-        x_2 : torch.Tensor, shape=[n_2_cells, channels]
-            Input features on the faces of the cell complex.
-            Optional, only required if attention is used between edges and faces.
+        x: torch.Tensor, shape=[n_nodes, channels]
+            Input features on the nodes of the simplicial complex.
+        incidence_1 : torch.sparse, shape=[n_nodes, n_edges]
+            Incidence matrix :math:`B_1` mapping edges to nodes.
+        adjacency_0 : torch.sparse, shape=[n_nodes, n_nodes]
+            Adjacency matrix :math:`A_0^{\uparrow}` mapping nodes to nodes via edges.
 
         Returns
         -------
-        _ : torch.Tensor, shape=[1, num_classes]
-            Output prediction on the entire cell complex.
+        _ : torch.Tensor, shape=[n_nodes, channels]
+            Output features on the nodes of the simplicial complex.
         """
-        x_0 = torch.nn.functional.relu(x_0)
-        x_1 = torch.nn.functional.relu(x_1)
+        incidence_1_transpose = incidence_1.transpose(1, 0)
 
-        x_0 = self.conv_0_to_0(x_0, neighborhood_0_to_0)
-        x_0 = torch.nn.functional.relu(x_0)
+        x_0_level1 = self.conv_level1_0_to_0(x_0, adjacency_0)
+        x_1_level1 = self.conv_level1_0_to_1(x_0, incidence_1_transpose)
 
-        x_2 = self.conv_1_to_2(x_1, neighborhood_1_to_2, x_2)
-        x_2 = torch.nn.functional.relu(x_2)
+        x_0_level2 = self.conv_level2_0_to_0(x_0_level1, adjacency_0)
+        x_1_level2 = self.conv_level2_1_to_0(x_1_level1, incidence_1)
 
-        return x_0, x_1, 
-  
-  
-  
+        x_0 = self.aggr_on_nodes([x_0_level2, x_1_level2])
+        return x_0
