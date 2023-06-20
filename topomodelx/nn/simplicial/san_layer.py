9a4e483588258240c1d766341ddfecccf5934ed7
gbg141 <guille_gbg@hotmail.com>
2023-06-20 18:44:59 +0100

    SAN rewritten

--- topomodelx/nn/simplicial/san_layer.py
+++ topomodelx/nn/simplicial/san_layer.py
@@ -1,60 +1,64 @@
 """Simplicial Attention Network (SAN) Layer."""
 import torch
 from torch.nn import functional as F
+from torch.nn.parameter import Parameter
+
 from topomodelx.base.aggregation import Aggregation
 from topomodelx.base.conv import Conv
 
 
 class SANLayer(torch.nn.Module):
-    """
-
-    """
+    r"""Class for the SAN layer."""
 
     def __init__(
         self,
         channels_in,
         channels_out,
-        J=2, # approximation order
+        J=2,  # approximation order
+        J_har=5,  # approximation order for harmonic
+        epsilon_har=1e-1,  # epsilon for harmonic, it takes into account the normalization
     ):
         super().__init__()
-        
-        #self.Lup, self.Ld, self.P = Lup, Ld, P
-        
+
         self.J = J
+        self.J_har = J_har
+        self.epsilon_har = epsilon_har
         self.channels_in = channels_in
         self.channels_out = channels_out
 
         self.att_slice = self.channels_out * self.J
-        
-        # Cell Convolution
 
-        self.W_irr =  [
-                        torch.nn.Linear(in_features=self.channels_in,
-                                        out_features=self.channels_out)
-                        for _ in range(self.J)
-                        ]
+        #  Convolution
+        self.weight_irr = Parameter(
+            torch.Tensor(self.J, self.channels_in, self.channels_out)
+        )
+        self.weight_sol = Parameter(
+            torch.Tensor(self.J, self.channels_in, self.channels_out)
+        )
+        self.weight_har = Parameter(torch.Tensor(self.channels_in, self.channels_out))
+        # self.W_irr =  [
+        #                torch.nn.Linear(in_features=self.channels_in,
+        #                                out_features=self.channels_out)
+        #                for _ in range(self.J)
+        #                ]
 
-        self.W_sol = [
-                        torch.nn.Linear(in_features=self.channels_in,
-                                        out_features=self.channels_out)
-                            for _ in range(self.J)
-                          ]
-        
-        self.W_har = torch.nn.Linear(in_features=self.channels_in,
-                                    out_features=self.channels_out)
-        
+        # self.W_sol = [
+        #                torch.nn.Linear(in_features=self.channels_in,
+        #                                out_features=self.channels_out)
+        #                    for _ in range(self.J)
+        #                  ]
+
+        self.W_har = torch.nn.Linear(
+            in_features=self.channels_in, out_features=self.channels_out
+        )
+
         # Attention
-        self.att_irr = torch.nn.Parameter(
-                torch.Tensor(2 * self.att_slice, 1)
-            )
-        
-        self.att_sol = torch.nn.Parameter(
-                torch.Tensor(2 * self.att_slice, 1)
-            )
-        
+        self.att_irr = Parameter(torch.Tensor(2 * self.att_slice, 1))
+        self.att_sol = Parameter(torch.Tensor(2 * self.att_slice, 1))
+
         # self.att_irr = torch.nn.Parameter(torch.empty(size=(2 * self.att_slice, 1)))
         # self.att_sol = torch.nn.Parameter(torch.empty(size=(2 * self.att_slice, 1)))
-        
+
         # self.bin_mask_Lu = None
         # self.bin_mask_Ld = None
 
@@ -64,73 +68,84 @@ class SANLayer(torch.nn.Module):
     #     gain = torch.nn.init.calculate_gain('relu')
     #     torch.nn.init.xavier_uniform_(self.conv_down.weight, gain=gain)
     #     torch.nn.init.xavier_uniform_(self.conv_up.weight, gain=gain)
-        
-    def forward(self, x, Lup, Ld, P):
+
+    def forward(self, x, Lup, Ldown, P):
         r"""Forward pass.
 
         The forward pass was initially proposed in [HRGZ22]_.
         Its equations are given in [TNN23]_ and graphically illustrated in [PSHM23]_.
-
-        .. math::
-           
-
-        References
-        ----------
-        
-
-        Parameters
-        ----------
-        
-        Returns
-        -------
-        
         """
-        
-        x_irr = torch.cat([self.W_irr[i](x) for i in range(self.J)], dim=1)
-        x_sol = torch.cat([self.W_sol[i](x) for i in range(self.J)], dim=1)
-
+        h_irr = torch.matmul(x, self.weight_irr)
+        h_sol = torch.matmul(x, self.weight_sol)
+        # x_irr = torch.cat([torch.mm(x,self.weight_irr[i]) for i in range(self.J)], dim=1)
+        # x_sol = torch.cat([torch.mm(x,self.weight_sol[i]) for i in range(self.J)], dim=1)
 
         # Broadcast add
         # Attention map
-        
+
         # (Ex1) + (1xE) -> (ExE)
-        E_irr = F.leaky_relu((x_irr @ self.att_irr[:self.att_slice, :]) + 
-                                (x_irr @ self.att_irr[self.att_slice:, :]).T) 
-        
-        # (Ex1) + (1xE) -> (ExE) 
-        E_sol = F.leaky_relu((x_sol @ self.att_sol[:self.att_slice, :]) + 
-                                (x_sol @ self.att_sol[self.att_slice:, :]).T)  
-        
+        e_irr = F.leaky_relu(
+            torch.mm(
+                h_irr.reshape(-1, self.J * self.channels_out),
+                self.att_irr[: self.att_slice, :],
+            )
+            + torch.mm(
+                h_irr.reshape(-1, self.J * self.channels_out),
+                self.att_irr[self.att_slice :, :],
+            ).T
+        )
+        e_sol = F.leaky_relu(
+            torch.mm(
+                h_sol.reshape(-1, self.J * self.channels_out),
+                self.att_sol[: self.att_slice, :],
+            )
+            + torch.mm(
+                h_sol.reshape(-1, self.J * self.channels_out),
+                self.att_sol[self.att_slice :, :],
+            ).T
+        )
 
         # Consider only ones which have connections
-        MASK_D, MASK_U = Ld != 0, Lup != 0
-        E_irr[~MASK_D] = float("-1e20")
-        E_sol[~MASK_U] = float("-1e20")
+        alpha_irr = torch.sparse.softmax(e_irr.sparse_mask(Ldown), dim=1).to_dense()
+        alpha_sol = torch.sparse.softmax(e_sol.sparse_mask(Lup), dim=1).to_dense()
+        # MASK_D, MASK_U = Ldown != 0, Lup != 0
+        # e_irr[~MASK_D] = float("-1e20")
+        # e_sol[~MASK_U] = float("-1e20")
 
         # Optional dropout
         # (ExE) -> (ExE)
-        alpha_irr = F.softmax(E_irr, dim=-1)
-                               
-        # (ExE) -> (ExE)
-        alpha_sol = F.softmax(E_sol, dim=-1) 
+        # alpha_irr = F.softmax(e_irr, dim=-1)
+        # alpha_sol = F.softmax(e_sol, dim=-1)
 
+        alpha_exp_irr = alpha_irr.unsqueeze(0)
+        alpha_exp_sol = alpha_sol.unsqueeze(0)
+        for p in range(self.J - 1):
+            alpha_exp_irr = torch.cat(
+                [alpha_exp_irr, torch.mm(alpha_exp_irr[p], alpha_irr).unsqueeze(0)],
+                dim=0,
+            )
+            alpha_exp_sol = torch.cat(
+                [alpha_exp_sol, torch.mm(alpha_exp_sol[p], alpha_sol).unsqueeze(0)],
+                dim=0,
+            )
+        z_irr = torch.sum(torch.matmul(alpha_exp_irr.to_dense(), h_irr))
+        z_sol = torch.sum(torch.matmul(alpha_exp_sol.to_dense(), h_sol))
 
-        
-        z_i = self.W_irr[0](torch.matmul(alpha_irr, x))
-        z_s = self.W_sol[0](torch.matmul(alpha_sol, x))  
-        for p in range(1, self.J):
-            alpha_irr = torch.matmul(alpha_irr, Ld)
-            alpha_sol = torch.matmul(alpha_sol, Lup)
-            
-            # (ExE) x (ExF_out) -> (ExF_out)
-            z_i = z_i + self.W_irr[p](torch.matmul(alpha_irr, x))
-            # (ExE) x (ExF_out) -> (ExF_out)
-            z_s = z_s + self.W_sol[p](torch.matmul(alpha_sol, x))  
+        # WRONG!! Alphas should be multiplied by themselves, not by Laplacians!!!!
+        # z_i = torch.mm(torch.matmul(alpha_irr, x),self.weight_irr[0])
+        # z_s = torch.mm(torch.matmul(alpha_sol, x),self.weight_sol[0])
+        # for p in range(1, self.J):
+        #    alpha_irr = torch.matmul(alpha_irr, Ldown)
+        #    alpha_sol = torch.matmul(alpha_sol, Lup)
 
-        
-        # Harmonic
-        z_har = self.W_har(torch.matmul(P, x))
-        
+        # (ExE) x (ExF_out) -> (ExF_out)
+        #    z_i = z_i + torch.mm(torch.matmul(alpha_irr, x),self.weight_irr[p])
+        # (ExE) x (ExF_out) -> (ExF_out)
+        #    z_s = z_s + torch.mm(torch.matmul(alpha_sol, x),self.weight_sol[p])
+
+        # Harmonic component
+        z_har = torch.mm(torch.matmul(P, x), self.weight_har)
+
         # final output
-        x = (z_i + z_s + z_har)
+        x = z_irr + z_sol + z_har
         return x
