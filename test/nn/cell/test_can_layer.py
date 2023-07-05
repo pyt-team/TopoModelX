93580f7a7bc0e4dc4c0693eb358b77cf358c9820
L-Scofano <luca.scofano@uniroma1.it>
2023-07-05 12:07:45 +0200

    add new tests

--- test/nn/cell/test_can_layer.py
+++ test/nn/cell/test_can_layer.py
@@ -9,6 +9,51 @@ from topomodelx.nn.cell.can_layer import CANLayer
 class TestCANLayer:
     """Unit tests for the CANLayer class."""
 
+    def test_reset_parameters(self):
+        """Test the reset_parameters method of CANLayer."""
+        in_channels = 2
+        out_channels = 5
+
+        can_layer = CANLayer(
+            in_channels=in_channels,
+            out_channels=out_channels,
+        )
+        can_layer.reset_parameters()
+
+        for module in can_layer.modules():
+            if hasattr(module, "reset_parameters"):
+                module.reset_parameters()
+    
+    def test_default_parameters(self):
+        """Test the default parameters of CANLayer."""
+        layer = CANLayer(in_channels=16, out_channels=32)
+        assert layer is not None
+
+    def test_skip_connection_false(self):
+        """Test without skip connection."""
+        layer = CANLayer(in_channels=16, out_channels=32, skip_connection=False)
+        assert layer is not None
+    
+    def test_concat_false(self):
+        """Test without concatenation."""
+        layer = CANLayer(in_channels=16, out_channels=32, concat=False)
+        assert layer is not None
+    
+    def test_heads(self):
+        """Test with multiple heads."""
+        layer = CANLayer(in_channels=16, out_channels=32, heads=3)
+        assert layer is not None
+    
+    def test_dropout(self):
+        """Test with dropout."""
+        layer = CANLayer(in_channels=16, out_channels=32, dropout=0.5)
+        assert layer is not None
+    
+    def test_in_channels_0(self):
+        """Test with in_channels_0."""
+        with pytest.raises(ValueError):
+            layer = CANLayer(in_channels=0, out_channels=32)
+
     def test_forward(self):
         """Test the forward method of CANLayer."""
         # set seed for reproducibility
@@ -45,17 +90,3 @@ class TestCANLayer:
         else:
             assert x_1.shape == (n_cells, out_channels)
 
-    def test_reset_parameters(self):
-        """Test the reset_parameters method of CANLayer."""
-        in_channels = 2
-        out_channels = 5
-
-        can_layer = CANLayer(
-            in_channels=in_channels,
-            out_channels=out_channels,
-        )
-        can_layer.reset_parameters()
-
-        for module in can_layer.modules():
-            if hasattr(module, "reset_parameters"):
-                module.reset_parameters()
