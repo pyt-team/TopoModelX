2cd596d9e96fd38284f5008c4cb1455813b77e66
Pavlo Melnyk <pavlo.o.melnyk@gmail.com>
2023-07-08 17:49:33 +0200

    fix formatting

--- test/nn/simplicial/test_sccn_layer.py
+++ test/nn/simplicial/test_sccn_layer.py
@@ -16,13 +16,21 @@ class TestSCCNLayer:
         n_rank_0_cells = 11
         n_rank_1_cells = 22
 
-        incidences = {"rank_1": 2 * torch.randint(0, 2, (n_rank_0_cells, n_rank_1_cells)).float() - 1}
-                      
-        adjacencies = {"rank_0": torch.eye(n_rank_0_cells).float(),
-                       "rank_1": 2 * torch.randint(0, 2, (n_rank_1_cells, n_rank_1_cells)).float() - 1}
+        incidences = {
+            "rank_1": 2 * torch.randint(0, 2, (n_rank_0_cells, n_rank_1_cells)).float()
+            - 1
+        }
 
-        features = {"rank_0": torch.randn(n_rank_0_cells, channels), 
-                    "rank_1": torch.randn(n_rank_1_cells, channels)}
+        adjacencies = {
+            "rank_0": torch.eye(n_rank_0_cells).float(),
+            "rank_1": 2 * torch.randint(0, 2, (n_rank_1_cells, n_rank_1_cells)).float()
+            - 1,
+        }
+
+        features = {
+            "rank_0": torch.randn(n_rank_0_cells, channels),
+            "rank_1": torch.randn(n_rank_1_cells, channels),
+        }
 
         sccn = SCCNLayer(channels, max_rank)
         output = sccn.forward(features, incidences, adjacencies)
@@ -43,11 +51,14 @@ class TestSCCNLayer:
             if isinstance(module, topomodelx.base.conv.Conv):
                 try:
                     torch.testing.assert_allclose(
-                        module.weight,  torch.nn.init.xavier_uniform_(module.weight.clone(), gain=1.414)
+                        module.weight,
+                        torch.nn.init.xavier_uniform_(
+                            module.weight.clone(), gain=1.414
+                        ),
                     )
                     # Raise AssertionError if parameters have not changed after the reset
                     raise AssertionError("Parameters have not changed after the reset")
-                
+
                 except AssertionError as ae:
                     # This is expected if parameters have changed
                     pass
