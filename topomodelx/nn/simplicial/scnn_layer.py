f18bb9dd71acabdab1cbb4f17b70df89cc4b5982
cookbook-ms <m.yang-2@tudelft.nl>
2023-07-10 13:01:50 +0200

    Update scnn_layer.py

--- topomodelx/nn/simplicial/scnn_layer.py
+++ topomodelx/nn/simplicial/scnn_layer.py
@@ -48,16 +48,19 @@ class SCNNLayer(torch.nn.Module):
       - to implement Lap_down@Lap_down@X, we consider chebyshev 
         method to avoid matrix@matrix computation
     """
-    def __init__(self, 
-                 in_channels,
-                 out_channels,
-                 conv_order_down,
-                 conv_order_up,
-                 aggr_norm=False,
-                 update_func=None,
-                 initialization="xavier_uniform"):
+
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        conv_order_down,
+        conv_order_up,
+        aggr_norm=False,
+        update_func=None,
+        initialization="xavier_uniform",
+    ):
         super().__init__()
-      
+
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.conv_order_down = conv_order_down
@@ -66,13 +69,17 @@ class SCNNLayer(torch.nn.Module):
         self.update_func = update_func
         self.initialization = initialization
         assert initialization in ["xavier_uniform", "xavier_normal"]
-      
+
         self.weight = Parameter(
-           torch.Tensor(self.in_channels, self.out_channels, 
-                        1+self.conv_order_down+self.conv_order_up))
-      
+            torch.Tensor(
+                self.in_channels,
+                self.out_channels,
+                1 + self.conv_order_down + self.conv_order_up,
+            )
+        )
+
         self.reset_parameters()
-      
+
     def reset_parameters(self, gain=1.414):
         r"""Reset learnable parameters.
 
@@ -95,18 +102,18 @@ class SCNNLayer(torch.nn.Module):
                 "Initialization method not recognized. "
                 "Should be either xavier_uniform or xavier_normal."
             )
-          
+
     def aggr_norm_func(self, conv_operator, x):
         r""" aggregation normalization 
         """
         neighborhood_size = torch.sum(conv_operator.to_dense(), dim=1)
-        neighborhood_size_inv = 1/neighborhood_size
+        neighborhood_size_inv = 1 / neighborhood_size
         neighborhood_size_inv[~(torch.isfinite(neighborhood_size_inv))] = 0
-        
+
         x = torch.einsum("i,ij->ij ", neighborhood_size_inv, x)
         x[~torch.isfinite(x)] = 0
-        return x 
-      
+        return x
+
     def update(self, x):
         """Update embeddings on each cell (step 4).
 
@@ -124,8 +131,8 @@ class SCNNLayer(torch.nn.Module):
             return torch.sigmoid(x)
         if self.update_func == "relu":
             return torch.nn.functional.relu(x)
-            
-    def chebyshev_conv(self, conv_operator, conv_order, x): 
+
+    def chebyshev_conv(self, conv_operator, conv_order, x):
         r"""A Chebyshev convolution method.
         Parameters
         ----------
@@ -145,12 +152,12 @@ class SCNNLayer(torch.nn.Module):
         X = torch.empty(size=(num_simplices, num_channels, conv_order))
         X[:, :, 0] = torch.mm(conv_operator, x)
         for k in range(1, conv_order):
-            X[:, :, k] = torch.mm(conv_operator, X[:, :, k-1])
+            X[:, :, k] = torch.mm(conv_operator, X[:, :, k - 1])
             if self.aggr_norm:
                 X[:, :, k] = self.aggr_norm_func(conv_operator, X[:, :, k])
-        
-        return X 
-        
+
+        return X
+
     def forward(self, x, laplacian_down, laplacian_up):
         r"""Forward computation. 
         
@@ -172,30 +179,28 @@ class SCNNLayer(torch.nn.Module):
           Output features on the edges of the simplical complex.
         """
 
-        num_simplices, _ = x.shape 
+        num_simplices, _ = x.shape
 
         identity = torch.eye(num_simplices)
 
-        x_identity = torch.unsqueeze(identity@x, 2)
+        x_identity = torch.unsqueeze(identity @ x, 2)
 
         if self.conv_order_down > 0 and self.conv_order_up > 0:
-            x_down = self.chebyshev_conv(laplacian_down, 
-                                         self.conv_order_down, x)
+            x_down = self.chebyshev_conv(laplacian_down, self.conv_order_down, x)
             x_up = self.chebyshev_conv(laplacian_up, self.conv_order_up, x)
             x = torch.cat((x_identity, x_down, x_up), 2)
-        elif self.conv_order_down > 0 and self.conv_order_up == 0: 
-            x_down = self.chebyshev_conv(laplacian_down, 
-                                         self.conv_order_down, x)
+        elif self.conv_order_down > 0 and self.conv_order_up == 0:
+            x_down = self.chebyshev_conv(laplacian_down, self.conv_order_down, x)
             x = torch.cat((x_identity, x_down), 2)
         elif self.conv_order_down == 0 and self.conv_order_up > 0:
             x_up = self.chebyshev_conv(laplacian_up, self.conv_order_up, x)
             x = torch.cat((x_identity, x_up), 2)
-        else: 
-            x = x_identity 
-          
-        y = torch.einsum('nik,iok->no', x, self.weight)
-        
+        else:
+            x = x_identity
+
+        y = torch.einsum("nik,iok->no", x, self.weight)
+
         if self.update_func is None:
             return y
-        
-        return self.update(y) 
+
+        return self.update(y)
