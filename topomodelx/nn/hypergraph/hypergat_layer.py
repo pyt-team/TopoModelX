f9e7ea941f92ef56cbaf3db1c660d6e611113464
Snopoff <snopoffp@gmail.com>
2023-07-12 13:27:21 +0300

    implemented forward

--- topomodelx/nn/hypergraph/hypergat_layer.py
+++ topomodelx/nn/hypergraph/hypergat_layer.py
@@ -1,10 +1,10 @@
 """HyperGAT layer."""
 import torch
 
-from topomodelx.base.conv import Conv
+from topomodelx.base.message_passing import MessagePassing
 
 
-class HyperGATLayer(torch.nn.Module):
+class HyperGATLayer(MessagePassing):
     """Implementation of the HyperGAT layer proposed in [DWLLL20].
 
     References
@@ -15,14 +15,97 @@ class HyperGATLayer(torch.nn.Module):
 
     Parameters
     ----------
-    in_features : int
+    in_channels : int
         Dimension of the input features.
-    out_features : int
+    out_channels : int
         Dimension of the output features.
 
     """
 
-    def __init__(self, in_features, out_features, *args, **kwargs) -> None:
-        super(HyperGATLayer, self).__init__(*args, **kwargs)
-        self.in_features = in_features
-        self.out_features = out_features
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        update_func="relu",
+        initialization="xavier_uniform",
+    ) -> None:
+        super().__init__(initialization=initialization)
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.update_func = update_func
+
+        self.weight1 = torch.nn.Parameter(
+            torch.Tensor(self.in_channels, self.out_channels)
+        )
+        self.weight2 = torch.nn.Parameter(
+            torch.Tensor(self.out_channels, self.out_channels)
+        )
+
+        self.att_weight1 = torch.nn.Parameter(torch.zeros(size=(2 * out_channels, 1)))
+        self.att_weight2 = torch.nn.Parameter(torch.zeros(size=(2 * out_channels, 1)))
+
+    def attention(self, x_source, x_target=None, mechanism="node-level"):
+        """Attention."""
+        x_source_per_message = x_source[self.source_index_j]
+        x_target_per_message = (
+            x_source[self.target_index_i]
+            if x_target is None
+            else x_target[self.target_index_i]
+        )
+
+        x_source_target_per_message = torch.functional.leaky_relu(
+            torch.cat([x_source_per_message, x_target_per_message], dim=1)
+        )
+
+        return torch.nn.functional.softmax(
+            torch.matmul(x_source_target_per_message, self.att_weight1)
+            if mechanism == "node-level"
+            else torch.matmul(x_source_target_per_message, self.att_weight2)
+        )
+
+    def update(self, x_message_on_target, x_target=None):
+        """Update embeddings on each cell (step 4).
+
+        Parameters
+        ----------
+        x_message_on_target : torch.Tensor, shape=[n_target_cells, out_channels]
+            Output features on target cells.
+
+        Returns
+        -------
+        _ : torch.Tensor, shape=[n_target_cells, out_channels]
+            Updated output features on target cells.
+        """
+        if self.update_func == "sigmoid":
+            return torch.sigmoid(x_message_on_target)
+        if self.update_func == "relu":
+            return torch.nn.functional.relu(x_message_on_target)
+
+    def forward(self, x_source, incidence):
+        """Forward."""
+        intra_aggregation = incidence @ x_source
+
+        neighborhood = incidence  # .coalesce()
+        self.target_index_i, self.source_index_j = neighborhood.indices()
+
+        attention_values = self.attention(intra_aggregation)
+        neighborhood = torch.sparse_coo_tensor(
+            indices=neighborhood.indices(),
+            values=attention_values * neighborhood.values(),
+            size=neighborhood.shape,
+        )
+        intra_aggregation_with_attention = neighborhood.t() @ x_source
+        hedge_representation = self.update(
+            intra_aggregation_with_attention @ self.weight1
+        )
+
+        inter_aggregation = incidence.t() @ hedge_representation
+
+        attention_values = self.attention(inter_aggregation, intra_aggregation)
+        neighborhood = torch.sparse_coo_tensor(
+            indices=neighborhood.indices(),
+            values=attention_values * neighborhood.values(),
+            size=neighborhood.shape,
+        )
+        intra_aggregation_with_attention = neighborhood.t() @ x_source
+        return self.update(intra_aggregation_with_attention @ self.weight2)
