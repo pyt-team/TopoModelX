7a54402bc82b45df86cf84f4a673cfb6c4ab31e7
Germandev55 <gera303813_96@mail.ru>
2023-07-12 13:42:36 +0300

    Update hypersage_layer.py

--- topomodelx/nn/hypergraph/hypersage_layer.py
+++ topomodelx/nn/hypergraph/hypersage_layer.py
@@ -6,7 +6,7 @@ class HyperSAGELayer(torch.nn.Module):
     """Implementation of the HyperSAGE layer proposed in [DWLLL20].
 
     References
-    ----------
+    -----------
     .. [AGRW20] Devanshu Arya, Deepak K Gupta, Stevan Rudinac and Marcel Worring. HyperSAGE:
         Generalizing inductive representation learning on hypergraphs. arXiv preprint arXiv:2010.04558. 2020
     """
