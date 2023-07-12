ae8bfc03edc8ee20cb4a957d92becfb47527e501
Snopoff <snopoffp@gmail.com>
2023-07-12 13:35:30 +0300

    initial commit for HyperSAGE layer

---
+++ test/nn/hypergraph/test_hypersage_layer.py
@@ -0,0 +1,11 @@
+"""Test the HyperSAGE layer."""
+import pytest
+import torch
+
+from topomodelx.nn.hypergraph.hypersage_layer import HyperSAGELayer
+
+
+class TestHyperSAGELayer:
+    """Test the HyperSAGE layer."""
+
+    pass
---
+++ topomodelx/nn/hypergraph/hypersage_layer.py
@@ -0,0 +1,14 @@
+"""HyperSAGE layer."""
+import torch
+
+
+class HyperSAGELayer(torch.nn.Module):
+    """Implementation of the HyperSAGE layer proposed in [DWLLL20].
+
+    References
+    ----------
+    .. [AGRW20] Devanshu Arya, Deepak K Gupta, Stevan Rudinac and Marcel Worring. HyperSAGE:
+        Generalizing inductive representation learning on hypergraphs. arXiv preprint arXiv:2010.04558. 2020
+    """
+
+    pass
