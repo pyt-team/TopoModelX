a6a5875939003e741f1186e649dbcbc167168adb
Tanya Malygina <merlettaia@gmail.com>
2023-07-13 23:53:16 +0200

    add dynamic topology implementation to DHGCN layer both with corresponding tests and simple tutorial

--- test/nn/hypergraph/test_dhgcn_layer.py
+++ test/nn/hypergraph/test_dhgcn_layer.py
@@ -11,8 +11,37 @@ class TestDHGCNLayer:
     @pytest.fixture
     def template_layer(self):
         """Return a DHGCN layer."""
-        return DHGCNLayer()
+        node_channels = 10
+        kn = 3
+        km = 4
+        return DHGCNLayer(
+            in_channels=node_channels,
+            intermediate_channels=node_channels,
+            out_channels=node_channels,
+            k_neighbours=kn,
+            k_centroids=km,
+        )
+
+    def test_kmeans(self, template_layer):
+        """Test the forward pass of the template layer."""
+        x_0 = torch.randn(4, 10)
+        output = template_layer.kmeans(x_0, k=template_layer.k_centroids)
+        assert output.shape == (2, 4)
+
+    def test_kmeans_with_invalid_input(self, template_layer):
+        """Test the forward pass of the template layer with invalid input."""
+        x_0 = torch.randn(4, 10)
+        with pytest.raises(ValueError):
+            template_layer.kmeans(x_0, k=5)
 
     def test_forward(self, dhgcn_layer):
         """Test the forward pass of the DHGCN layer."""
-        pass
+        x = torch.randn(4, 10)
+        output = dhgcn_layer.forward(x)
+        assert output.shape == (4, 10)
+
+    def test_forward_with_invalid_input(self, template_layer):
+        """Test the forward pass of the template layer with invalid input."""
+        x_2 = torch.randn(4, 10)
+        with pytest.raises(ValueError):
+            template_layer.forward(x_2)
