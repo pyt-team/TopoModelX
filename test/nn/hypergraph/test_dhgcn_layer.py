ace24cd73690e79faa36651f67cdc3602a4b7761
Tanya Malygina <merlettaia@gmail.com>
2023-07-14 02:10:44 +0200

    fix tests 2+add gpu test for kmeans method

--- test/nn/hypergraph/test_dhgcn_layer.py
+++ test/nn/hypergraph/test_dhgcn_layer.py
@@ -9,7 +9,7 @@ class TestDHGCNLayer:
     """Test the DHGCN layer."""
 
     @pytest.fixture
-    def template_layer(self):
+    def dhgcn_layer(self):
         """Return a DHGCN layer."""
         node_channels = 10
         kn = 3
@@ -22,20 +22,30 @@ class TestDHGCNLayer:
             k_centroids=km,
         )
 
-    def test_kmeans(self, template_layer):
+    def test_kmeans(self, dhgcn_layer):
         """Test the forward pass of the template layer."""
         x_0 = torch.randn(4, 10)
-        output = template_layer.kmeans(x_0, k=template_layer.k_centroids)
+        output = dhgcn_layer.kmeans(x_0, k=dhgcn_layer.k_centroids)
         assert output.shape == (2, 4)
+        if torch.cuda.is_available():
+            x_0 = x_0.to("gpu")
+            output = dhgcn_layer.kmeans(x_0, k=dhgcn_layer.k_centroids)
+            assert output.device.type == x_0.device.type
 
-    def test_kmeans_with_invalid_input(self, template_layer):
+    def test_kmeans_with_invalid_input(self, dhgcn_layer):
         """Test the forward pass of the template layer with invalid input."""
         x_0 = torch.randn(4, 10)
         with pytest.raises(ValueError):
-            template_layer.kmeans(x_0, k=5)
+            dhgcn_layer.kmeans(x_0, k=5)
 
     def test_forward(self, dhgcn_layer):
         """Test the forward pass of the DHGCN layer."""
         x = torch.randn(4, 10)
         output = dhgcn_layer.forward(x)
         assert output.shape == (4, 10)
+
+    def test_forward_invalid_input(self, dhgcn_layer):
+        """Test the forward pass of the DHGCN layer."""
+        x = torch.randn(2, 10)
+        with pytest.raises(ValueError):
+            dhgcn_layer.forward(x)
