9b729bd18cefad800a42c84485070ea9dfb06e58
User <User@Yixiaos-MacBook-Air.local>
2023-07-14 17:57:53 +0800

    SCN(added_test_resetparameter)

--- test/nn/simplicial/test_scn_layer.py
+++ test/nn/simplicial/test_scn_layer.py
@@ -35,3 +35,21 @@ class TestSCNLayer:
         assert x_0.shape == (n_0_cells, channels_0)
         assert x_1.shape == (n_1_cells, channels_1)
         assert x_2.shape == (n_2_cells, channels_2)
+
+    def test_reset_parameters(self):
+        """Test the reset of the parameters."""
+        channels_0 = 10
+        channels_1 = 20
+        channels_2 = 30
+
+        scn = SCNLayer(channels_0, channels_1, channels_2)
+        scn.reset_parameters()
+
+        for module in scn.modules():
+            if isinstance(module, torch.nn.Conv2d):
+                torch.testing.assert_allclose(
+                    module.weight, torch.zeros_like(module.weight)
+                )
+                torch.testing.assert_allclose(
+                    module.bias, torch.zeros_like(module.bias)
+                )
