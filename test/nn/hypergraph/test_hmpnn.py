57f824af65abc9f2c9ad914dd8d74139b2b7a301
Mustafa Hajij <mustafa.hajij@gmail.com>
2023-09-06 20:28:03 -0700

    fixing unit test for hmpnn

--- test/nn/hypergraph/test_hmpnn.py
+++ test/nn/hypergraph/test_hmpnn.py
@@ -1,5 +1,6 @@
 """Test the HMPNN class."""
 
+import numpy as np
 import torch
 
 from topomodelx.nn.hypergraph.hmpnn import HMPNN
@@ -14,6 +15,15 @@ class TestHMPNN:
         model = HMPNN(8, (8, 8), 1, 1).to(device)
 
         x_0 = torch.rand(8, 8)
+        x_1 = torch.rand(8, 8)
 
-        y = model(x_0)
-        assert y.shape == torch.Size([1])
+        adjacency_1 = torch.from_numpy(np.random.rand(8, 8)).to_sparse()
+
+        x_0, x_1 = (
+            torch.tensor(x_0).float().to(device),
+            torch.tensor(x_1).float().to(device),
+        )
+        adjacency_1 = adjacency_1.float().to(device)
+
+        y = model(x_0, x_1, adjacency_1)
+        assert y.shape == torch.Size([8, 1])
