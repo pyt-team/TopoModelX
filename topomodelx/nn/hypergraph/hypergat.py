fbbb5ab3f55a3ba94588ad3581204e55b524e525
levtelyatnikov <tlscabinet@hmail.com>
2023-11-02 15:18:42 +0000

    hypersage left

--- topomodelx/nn/hypergraph/hypergat.py
+++ topomodelx/nn/hypergraph/hypergat.py
@@ -10,12 +10,18 @@ class HyperGAT(torch.nn.Module):
 
     Parameters
     ----------
-    channels_edge : int
-        Dimension of edge features
-    channels_node : int
-        Dimension of node features
+    in_channels : int
+        Dimension of the input features.
+    hidden_channels : int
+        Dimension of the hidden features.
+    out_channels : int
+        Dimension of the output features.
     n_layer : int, default = 2
         Amount of message passing layers.
+    task_level: str, default="graph"
+        Level of the task. Either "graph" or "node".
+        If "graph", the output is pooled over all nodes in the hypergraph.
+
 
     References
     ----------
@@ -24,18 +30,26 @@ class HyperGAT(torch.nn.Module):
         https://aclanthology.org/2020.emnlp-main.399.pdf
     """
 
-    def __init__(self, in_channels, out_channels, n_layers=2):
+    def __init__(
+            self, 
+            in_channels,
+            hidden_channels,
+            out_channels, 
+            n_layers=2,
+            task_level="graph",
+        ):
         super().__init__()
         layers = []
-        layers.append(HyperGATLayer(in_channels=in_channels, out_channels=out_channels))
+        layers.append(HyperGATLayer(in_channels=in_channels, out_channels=hidden_channels))
         for _ in range(1, n_layers):
             layers.append(
-                HyperGATLayer(in_channels=out_channels, out_channels=out_channels)
+                HyperGATLayer(in_channels=hidden_channels, out_channels=hidden_channels)
             )
         self.layers = torch.nn.ModuleList(layers)
-        self.linear = torch.nn.Linear(out_channels, 1)
+        self.linear = torch.nn.Linear(hidden_channels, out_channels)
+        self.out_pool = True if task_level == "graph" else False
 
-    def forward(self, x_1, incidence_1):
+    def forward(self, x_0, incidence_1):
         """Forward computation through layers, then linear layer, then global max pooling.
 
         Parameters
@@ -53,6 +67,12 @@ class HyperGAT(torch.nn.Module):
             Label assigned to whole complex.
         """
         for layer in self.layers:
-            x_1 = layer.forward(x_1, incidence_1)
-        pooled_x = torch.max(x_1, dim=0)[0]
-        return torch.sigmoid(self.linear(pooled_x))[0]
+            x_0 = layer.forward(x_0, incidence_1)
+        
+        # Pool over all nodes in the hypergraph 
+        if self.out_pool is True:
+            x = torch.max(x_0, dim=0)[0]
+        else:
+            x = x_0
+
+        return self.linear(x)
