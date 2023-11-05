b1bf55f3c3fa798a23173200292f8471562cbd8a
gbg141 <guille_gbg@hotmail.com>
2023-11-05 12:36:13 +0100

    SCoNe checked (except tests)

--- topomodelx/nn/simplicial/scone.py
+++ topomodelx/nn/simplicial/scone.py
@@ -135,35 +135,28 @@ class TrajectoriesDataset(Dataset):
 class SCoNe(nn.Module):
     """Neural network implementation of classification using SCoNe."""
 
-    def __init__(self, sc: SimplicialComplex, hidden_dims: list[int]) -> None:
+    def __init__(self, in_channels: int, hidden_channels: int, n_layers: int) -> None:
         super().__init__()
-        self.incidence_1 = torch.Tensor(sc.incidence_matrix(1).toarray())
-        self.incidence_2 = torch.Tensor(sc.incidence_matrix(2).toarray())
-        self.adjacency = torch.Tensor(sc.adjacency_matrix(0).toarray())
 
-        # Weights for the last layer
-        self.weights = nn.parameter.Parameter(torch.Tensor(hidden_dims[-1], 1))
-        nn.init.xavier_uniform_(self.weights)
-
-        self.hidden_dimensions = hidden_dims
-        self.laplacian = len(hidden_dims)
+        self.in_channels = in_channels
+        self.hidden_channels = hidden_channels
+        self.n_layers = n_layers
 
         # Stack multiple SCoNe layers with given hidden dimensions
-        self.layers = nn.ModuleList([SCoNeLayer(1, hidden_dims[0])])
-        for i in range(self.laplacian - 1):
-            self.layers.append(SCoNeLayer(hidden_dims[i], hidden_dims[i + 1]))
+        self.layers = nn.ModuleList(
+            [SCoNeLayer(self.in_channels, self.hidden_channels)]
+        )
+        for i in range(self.n_layers - 1):
+            self.layers.append(SCoNeLayer(self.hidden_channels, self.hidden_channels))
 
         # Initialize parameters
         for layer in self.layers:
             layer.reset_parameters()
 
-    def forward(self, x: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
+    def forward(
+        self, x: torch.Tensor, incidence_1: torch.Tensor, incidence_2: torch.Tensor
+    ) -> torch.Tensor:
         """Forward pass through the network."""
         for layer in self.layers:
-            x = layer(x, self.incidence_1, self.incidence_2)
-        # Last layer going from edges to nodes using the boundary operator
-        x = self.incidence_1 @ x @ self.weights
-        # Take softmax only over neighbors by setting the logits of non-neighbors to approximately -inf
-        x = x + (1e-15 + mask).log()
-        x = nn.functional.log_softmax(x, dim=1)
+            x = layer(x, incidence_1, incidence_2)
         return x
