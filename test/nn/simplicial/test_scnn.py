6fe28a1d1bb76ffd7c1250bcd5f4c4d11b79bc17
levtelyatnikov <tlscabinet@gmail.com>
2023-11-07 15:24:51 +0100

    corrected tests

--- test/nn/simplicial/test_scnn.py
+++ test/nn/simplicial/test_scnn.py
@@ -38,7 +38,7 @@ class TestSCNN:
         incidence_1 = simplicial_complex.incidence_matrix(rank=1)
         incidence_2 = simplicial_complex.incidence_matrix(rank=2)
         laplacian_0 = simplicial_complex.hodge_laplacian_matrix(rank=0, weight=True)
-        laplacian_down_1 = simplicial_complex.down_laplacian_matrix(rank=1, weight=True)
+        laplacian_down_1 = simplicial_complex.down_laplacian_matrix(rank=1)
         laplacian_up_1 = simplicial_complex.up_laplacian_matrix(rank=1)
         laplacian_2 = simplicial_complex.hodge_laplacian_matrix(rank=2, weight=True)
 
