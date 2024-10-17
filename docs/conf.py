a11b86550696d8c451b2d81b0316caa8df9bef3a
Florian Frantzen <florian.frantzen@rwth-aachen.de>
2024-10-17 15:12:54 +0200

    Clean-up sphinx config

--- docs/conf.py
+++ docs/conf.py
@@ -3,47 +3,38 @@
 import os
 import shutil
 
+# -- Project information -----------------------------------------------------
+
 project = "TopoModelX"
 copyright = "2022-2023, PyT-Team, Inc."
 author = "PyT-Team Authors"
 language = "en"
+
+# -- General configuration ---------------------------------------------------
 
 extensions = [
     "nbsphinx",
     "nbsphinx_link",
     "numpydoc",
     "sphinx.ext.autodoc",
-    "sphinx.ext.coverage",
-    "sphinx.ext.doctest",
     "sphinx.ext.githubpages",
     "sphinx.ext.intersphinx",
-    "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
     "sphinx_copybutton",
     "sphinx_gallery.load_style",
 ]
 
-html_context = {
-    "github_user": "pyt-team",
-    "github_repo": "TopoModelX",
-    "github_version": "main",
-    "doc_path": "docs",
-}
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 
-html_theme_options = {
-    "icon_links": [
-        {
-            "name": "GitHub",
-            "url": "https://github.com/pyt-team/TopoModelX",
-            "icon": "fa-brands fa-github",
-            "type": "fontawesome",
-        }
-   ],
-   "use_edit_page_button": True,
+intersphinx_mapping = {
+    "networkx": ("https://networkx.org/documentation/stable/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "python": ("https://docs.python.org/3/", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy/", None),
+    "toponetx": ("https://pyt-team.github.io/toponetx/", None),
 }
-
-html_show_sourcelink = False
 
 # Configure nbsphinx for notebook execution
 nbsphinx_execute_arguments = [
@@ -52,62 +43,7 @@ nbsphinx_execute_arguments = [
 ]
 
 nbsphinx_execute = "never"
-
 nbsphinx_allow_errors = True
-
-templates_path = ["_templates"]
-
-source_suffix = [".rst"]
-
-master_doc = "index"
-
-
-nbsphinx_prolog = r"""
-{% set docname = env.doc2path(env.docname, base=None) %}
-
-.. raw:: latex
-    \nbsphinxstartnotebook{\scriptsize\noindent\strut
-    \textcolor{gray}{The following section was generated from
-    \sphinxcode{\sphinxupquote{\strut {{ docname | escape_latex }}}} \dotfill}}
-    """
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
-
-pygments_style = None
-
-html_theme = "pydata_sphinx_theme"
-html_baseurl = "https://pyt-team.github.io/topomodelx/"
-htmlhelp_basename = "pyt-teamdoc"
-html_last_updated_fmt = "%c"
-
-latex_elements = {}
-
-
-latex_documents = [
-    (
-        master_doc,
-        "topomodelx.tex",
-        "TopoModelX Documentation",
-        "PyT-Team",
-        "manual",
-    ),
-]
-
-man_pages = [(master_doc, "topomodelx", "TopoModelX Documentation", [author], 1)]
-
-texinfo_documents = [
-    (
-        master_doc,
-        "topomodelx",
-        "TopoModelX Documentation",
-        author,
-        "topomodelx",
-        "One line description of project.",
-        "Miscellaneous",
-    ),
-]
-
-epub_title = project
-epub_exclude_files = ["search.html"]
 
 
 def copy_thumbnails():
@@ -175,21 +111,37 @@ nbsphinx_thumbnails = {
     "notebooks/simplicial/scone_train": "_thumbnails/simplicial/scone_train.png",
 }
 
-# configure intersphinx
-intersphinx_mapping = {
-    "networkx": ("https://networkx.org/documentation/stable/", None),
-    "numpy": ("https://numpy.org/doc/stable/", None),
-    "python": ("https://docs.python.org/3/", None),
-    "scipy": ("https://docs.scipy.org/doc/scipy/", None),
-    "toponetx": ("https://pyt-team.github.io/toponetx/", None),
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = "pydata_sphinx_theme"
+html_baseurl = "https://pyt-team.github.io/topomodelx/"
+
+html_context = {
+    "github_user": "pyt-team",
+    "github_repo": "TopoModelX",
+    "github_version": "main",
+    "doc_path": "docs",
 }
 
-# configure numpydoc
-numpydoc_validation_checks = {"all", "GL01", "ES01", "SA01", "EX01"}
-numpydoc_show_class_members = False
-numpydoc_class_members_toctree = False
+html_theme_options = {
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/pyt-team/TopoModelX",
+            "icon": "fa-brands fa-github",
+            "type": "fontawesome",
+        }
+    ],
+    "use_edit_page_button": True,
+}
+
+html_show_sourcelink = False
 
 # Exclude copy button from appearing over notebook cell numbers by using :not()
 # The default copybutton selector is `div.highlight pre`
 # https://github.com/executablebooks/sphinx-copybutton/blob/master/sphinx_copybutton/__init__.py#L82
 copybutton_selector = ":not(.prompt) > div.highlight pre"
+
+# -- Options for EPUB output -------------------------------------------------
+
+epub_exclude_files = ["search.html"]
